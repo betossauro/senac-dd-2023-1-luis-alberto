8b0367b432b01a96d5cb005696e04b1d89bc183b
Beto <pfvrbeto@gmail.com>
2023-03-13 11:12:21 -0300

    Atualização para testes de insert em telefone e endereço

--- src/main/java/executavel/ExecutavelTelefonia.java
+++ src/main/java/executavel/ExecutavelTelefonia.java
@@ -3,6 +3,8 @@ package executavel;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.telefone.TelefoneDAO;
+import model.dao.telefonia.EnderecoDAO;
 import model.vo.telefonia.Cliente;
 import model.vo.telefonia.Endereco;
 import model.vo.telefonia.Telefone;
@@ -11,24 +13,44 @@ public class ExecutavelTelefonia {
 
 	public static void main(String[] args) {
 
-		Endereco endereco1 = new Endereco("88000123", "Mauro Ramos", "10", "Centro", "Florianópolis", "SC");
-
-		List<Telefone> telefonesDoSocrates = new ArrayList<Telefone>();
-		Telefone telefone1 = new Telefone("48", "32328888", true, false);
-		telefonesDoSocrates.add(telefone1);
-		telefonesDoSocrates.add(new Telefone("48", "98881234", true, true));
-
-		Cliente pele = new Cliente("Edson Arantes", "11122233344", true, null, endereco1);
-		Cliente socrates = new Cliente("Sócrates Brasileiro", "33322233344", true, telefonesDoSocrates, endereco1);
-
-		List<Cliente> clientes = new ArrayList<Cliente>();
-		clientes.add(pele);
-		clientes.add(socrates);
-
-		System.out.println("--------------- Clientes ---------------");
-		for (Cliente c : clientes) {
-			System.out.println(c.toString());
+//		Endereco endereco1 = new Endereco(null, "88000123", "Mauro Ramos", "50", "Centro", "Florianópolis", "SC");
+		Telefone telefone1 = new Telefone(null, null, "48", "998577999", false, true);
+		
+		
+//		EnderecoDAO salvadorDeEnderecos = new EnderecoDAO();
+//		salvadorDeEnderecos.inserir(endereco1);
+//		
+//		if (endereco1.getId() != null) {
+//			System.out.println("Novo endereço cadastrado!");
+//		} else {
+//			System.out.println("Erro ao cadastrar endereço!");
+//		}
+		
+		TelefoneDAO salvadorDeTelefones = new TelefoneDAO();
+		salvadorDeTelefones.inserir(telefone1);
+		
+		if (telefone1.getId() != null) {
+			System.out.println("Novo telefone cadastrado!");
+		} else {
+			System.out.println("Erro ao cadastrar telefone!");
 		}
+
+//		List<Telefone> telefonesDoSocrates = new ArrayList<Telefone>();
+//		Telefone telefone1 = new Telefone(null, null, "48", "32328888", true, false);
+//		telefonesDoSocrates.add(telefone1);
+//		telefonesDoSocrates.add(new Telefone(null, null, "48", "98881234", true, true));
+//
+//		Cliente pele = new Cliente(null, "Edson Arantes", "11122233344", true, null, endereco1);
+//		Cliente socrates = new Cliente(null, "Sócrates Brasileiro", "33322233344", true, telefonesDoSocrates, endereco1);
+//
+//		List<Cliente> clientes = new ArrayList<Cliente>();
+//		clientes.add(pele);
+//		clientes.add(socrates);
+//
+//		System.out.println("--------------- Clientes ---------------");
+//		for (Cliente c : clientes) {
+//			System.out.println(c.toString());
+//		}
 	}
 
 }
