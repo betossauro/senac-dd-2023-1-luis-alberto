36f2037a313190da2e78d091a5ee243b213d08a2
Beto <pfvrbeto@gmail.com>
2023-03-13 09:53:22 -0300

    create telefonia_insert.sql

---
+++ scripts/telefonia_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO ENDERECO (RUA, CEP, BAIRRO, CIDADE, ESTADO, NUMERO) 
+VALUES ('Mauro Ramos', '88000123', 'Centro', 'Florianópolis', 'SC', '10');
+
+INSERT INTO ENDERECO (RUA, CEP, BAIRRO, CIDADE, ESTADO, NUMERO) 
+VALUES ('Felipe Schimidt', '88500100', 'Centro', 'Florianópolis', 'SC', '50');
