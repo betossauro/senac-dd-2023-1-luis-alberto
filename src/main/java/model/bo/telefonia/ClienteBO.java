653a21805073d7e563479fcf62f2a8336e68bff0
Beto <pfvrbeto@gmail.com>
2023-04-16 10:38:47 -0300

    update add default insert -> setativo(true)

--- src/main/java/model/bo/telefonia/ClienteBO.java
+++ src/main/java/model/bo/telefonia/ClienteBO.java
@@ -13,58 +13,57 @@ import java.util.List;
 
 public class ClienteBO {
 
-    private ClienteDAO dao = new ClienteDAO();
+	private ClienteDAO dao = new ClienteDAO();
 
-    public Cliente inserir(Cliente novoCliente) throws CpfJaUtilizadoException, EnderecoInvalidoException {
-        if (dao.cpfJaUtilizado(novoCliente.getCpf())) {
-            throw new CpfJaUtilizadoException("CPF informado já foi cadastrado");
-        }
+	public Cliente inserir(Cliente novoCliente) throws CpfJaUtilizadoException, EnderecoInvalidoException {
+		if (dao.cpfJaUtilizado(novoCliente.getCpf())) {
+			throw new CpfJaUtilizadoException("CPF informado já foi cadastrado");
+		}
+		if (novoCliente.getEndereco() == null || novoCliente.getEndereco().getId() == null) {
+			throw new EnderecoInvalidoException("Endereço não informado");
+		}
+		novoCliente.setAtivo(true);
+		return dao.inserir(novoCliente);
+	}
 
-        if (novoCliente.getEndereco() == null || novoCliente.getEndereco().getId() == null) {
-            throw new EnderecoInvalidoException("Endereço não informado");
-        }
+	public boolean atualizar(Cliente clienteAlterado) throws EnderecoInvalidoException, CpfAlteradoException {
+		Cliente clienteOriginal = dao.consultarPorId(clienteAlterado.getId());
+		// Caso cliente possua telefone(s): lançar ClienteComTelefoneException
+		if (clienteAlterado.getCpf() != clienteOriginal.getCpf()) {
+			throw new CpfAlteradoException("CPF não pode ser alterado");
+		}
+		// Caso contrário: chamar dao.excluir(id)
+		validarEndereco(clienteAlterado);
+		return dao.atualizar(clienteAlterado);
+	}
 
-        return dao.inserir(novoCliente);
-    }
+	public boolean excluir(int id) throws ClienteComTelefoneException {
+		TelefoneDAO telefoneDao = new TelefoneDAO();
+		List<Telefone> telefonesDoCliente = telefoneDao.consultarPorIdCliente(id);
+		if (telefonesDoCliente != null && !telefonesDoCliente.isEmpty()) {
+			throw new ClienteComTelefoneException("Cliente não pode ser excluído pois possui telefone associado");
+		}
+		return dao.excluir(id);
 
-    public boolean atualizar(Cliente clienteAlterado) throws EnderecoInvalidoException, CpfAlteradoException {
-        Cliente clienteOriginal = dao.consultarPorId(clienteAlterado.getId());
-        //Caso cliente possua telefone(s): lançar ClienteComTelefoneException
-        if(clienteAlterado.getCpf() != clienteOriginal.getCpf()) {
-            throw new CpfAlteradoException("CPF não pode ser alterado");
-        }
-        //Caso contrário: chamar dao.excluir(id)
-        validarEndereco(clienteAlterado);
-        return dao.atualizar(clienteAlterado);
-    }
-
-    public boolean excluir(int id) throws ClienteComTelefoneException {
-        TelefoneDAO telefoneDao = new TelefoneDAO();
-        List<Telefone> telefonesDoCliente = telefoneDao.consultarPorIdCliente(id);
-        if (telefonesDoCliente!= null && !telefonesDoCliente.isEmpty()) {
-            throw new ClienteComTelefoneException("Cliente não pode ser excluído pois possui telefone associado");
-        }
-        return dao.excluir(id);
-
-        // OU
+		// OU
 //        Cliente clienteBuscado = dao.consultarPorId(id);
 //        if (!clienteBuscado.getTelefones().isEmpty()) {
 //            throw new ClienteComTelefoneException("Cliente não pode ser excluído pois possui telefone associado");
 //        }
 //        return dao.excluir(id);
-    }
+	}
 
-    public Cliente consultarPorId(int id) {
-        return dao.consultarPorId(id);
-    }
+	public Cliente consultarPorId(int id) {
+		return dao.consultarPorId(id);
+	}
 
-    public List<Cliente> consultarTodos() {
-        return dao.consultarTodos();
-    }
+	public List<Cliente> consultarTodos() {
+		return dao.consultarTodos();
+	}
 
-    private void validarEndereco(Cliente cliente) throws EnderecoInvalidoException {
-        if(cliente.getEndereco() == null || cliente.getEndereco().getId() == null) {
-            throw new EnderecoInvalidoException("Endereço não informado");
-        }
-    }
+	private void validarEndereco(Cliente cliente) throws EnderecoInvalidoException {
+		if (cliente.getEndereco() == null || cliente.getEndereco().getId() == null) {
+			throw new EnderecoInvalidoException("Endereço não informado");
+		}
+	}
 }
